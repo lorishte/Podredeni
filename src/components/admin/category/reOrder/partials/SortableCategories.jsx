96a893f72c2b5b803063e3968ca81fc4232da561
MJordanov81 <marian.jordanov@gmail.com>
2019-07-18 17:54:37 +0300

    Refactoring

---
+++ src/components/admin/category/reOrder/partials/SortableCategories.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+
+const SortableItem = SortableElement(({itemIndex, value}) =>
+
+	<tr className="text-center">
+		<td className="padding">
+			{itemIndex}
+		</td>
+
+		<td className="text-left">
+			{value.name}
+		</td>
+	</tr>);
+
+const SortableList = SortableContainer(({items}) => {
+	let itemIndex = 1;
+
+	return (
+		<tbody>
+		{items.map((value, index ) => (
+			<SortableItem key={`item-${index}`}
+			              itemIndex={itemIndex++}
+			              index={index}
+			              value={value}/>
+		))}
+		</tbody>
+	);
+});
+
+class SortableCategories extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			items: []
+		}
+	}
+
+	componentDidMount () {
+		this.setState({items: this.props.sortableItems})
+	}
+
+	onSortEnd = ({oldIndex, newIndex}) => {
+		this.setState({
+			items: arrayMove(this.state.items, oldIndex, newIndex),
+		});
+
+		this.props.handleOrderChange(this.state.items);
+	};
+
+	render () {
+
+		let itemsList;
+
+		
+
+		if(this.state.items.length > 0){
+
+			console.log(this.state.items);
+			
+			itemsList = (<SortableList
+				getContainer={() => document.getElementById('admin-sortable')}
+				items={this.state.items}
+				distance={10}
+				onSortEnd={this.onSortEnd}/>);
+		}
+
+		return <Table striped bordered condensed hover id="admin-products-table">{itemsList}</Table>;
+	}
+}
+
+export default SortableCategories;
