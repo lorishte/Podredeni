7c4796b27d93117f6b1452a3b4f9bba3310eb55a
MJordanov81 <marian.jordanov@gmail.com>
2020-04-28 12:27:30 +0300

    MiscDataService added

---
+++ src/services/miscData/miscDataService.js
@@ -0,0 +1,40 @@
+import requesterService from '../requester';
+
+const miscDataEndPoint = '/miscData';
+const auth = 'admin';
+
+export default {
+
+    loadMiscData: (key) => {
+
+        let endPoint = miscDataEndPoint + '/' + key;
+
+        return requesterService.get(endPoint);
+
+    },
+
+    updateMiscData: (key, value) => {
+
+        let endPointKey = miscDataEndPoint + '/' + key;
+
+        return requesterService
+            .update(endPointKey, auth, value);
+    },
+
+    createMiscData: (key, value) => {
+
+        let miscData = generateMiscData(key, value);
+
+        return requesterService
+            .post(miscDataEndPoint, auth, miscData);
+    },
+};
+
+function generateMiscData (key, value) {
+
+    return {
+        Key: key,
+        Value: value
+    }
+}
+
